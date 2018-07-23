4231c2df62eac783f954a6d56783a045f75e908e
karol <19kkarol96@gmail.com>
2018-07-23 13:53:07 +0200

    FIFO bytow sendera zrob w klasie

--- sikradio_sender.cpp
+++ sikradio_sender.cpp
@@ -22,10 +22,11 @@
 
 using namespace std;
 
+
 //./zadanie2 -a "123.123.123.123" -P 1234 -n "spitfire nazwa odbiornika" -C 4444  -p 900 -f 300 -R 250
 //write_sikradio_sender_arguments(mcast_addr, nazwa_odbiornika, data_port, ctrl_port, psize, fsize, rtime);
 void write_sikradio_sender_arguments(string mcast_addr, string nazwa_odbiornika,
-                                     uint32_t data_port, uint32_t ctrl_port,
+                                     string data_port, uint32_t ctrl_port,
                                      uint32_t psize, uint32_t fsize, uint32_t rtime) {
     printf("sikradio_sender arguments:\n\
     mcast_addr: %s\n\
@@ -42,7 +43,7 @@ void write_sikradio_sender_arguments(string mcast_addr, string nazwa_odbiornika,
 
 void set_sikradio_sender_arguments(const int& argc, char **argv,
                            string& mcast_addr, string& nazwa_odbiornika,
-                           uint32_t& data_port, uint32_t& ctrl_port,
+                           string& data_port, uint32_t& ctrl_port,
                            uint32_t& psize, uint32_t& fsize, uint32_t& rtime) {
     int c;
     opterr = 0;
@@ -53,7 +54,7 @@ void set_sikradio_sender_arguments(const int& argc, char **argv,
                 mcast_addr = string(optarg);
                 break;
             case 'P':
-                data_port = parse_optarg_to_number('P', optarg);
+                data_port = string(optarg);
                 break;
             case 'n':
                 nazwa_odbiornika = string(optarg);
@@ -92,12 +93,52 @@ void set_sikradio_sender_arguments(const int& argc, char **argv,
 
 
 
+void send_input_to_multicast(const string& addres, const string& data_port, const uint32_t psize,
+                             Input_fifo_queue& input_queue) {
+    static bool socket_initialized = false;
+    static int sockfd;
+    static struct addrinfo hints, *servinfo, *p;
+    static int rv;
+    static int numbytes;
+
+    if(!socket_initialized) {
+        memset(&hints, 0, sizeof hints);
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_DGRAM;
+
+        if ((rv = getaddrinfo(addres.c_str(), data_port.c_str(), &hints, &servinfo)) != 0) {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+            exit(1);
+        }
+        // loop through all the results and make a socket
+        for(p = servinfo; p != NULL; p = p->ai_next) {
+            if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                                 p->ai_protocol)) == -1) {
+                perror("talker: socket");
+                continue;
+            }
+
+            break;
+        }
+        if (p == NULL) {
+            fprintf(stderr, "talker: failed to create socket\n");
+            exit(1);
+        }
+        freeaddrinfo(servinfo);
+    }
+    
+    if ((numbytes = sendto(sockfd, argv[2], sizeof(char)*psize, 0,
+                           p->ai_addr, p->ai_addrlen)) == -1) {
+        perror("talker: sendto");
+        exit(1);
+    }
 
 
 
+    printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
+    close(sockfd);
 
-void send_input_to_multicast(Input_fifo_queue input_queue) {
-    input_queue.read_input();
+
 
 
 }
@@ -121,7 +162,7 @@ Input_fifo_queue input_queue;
 int main (int argc, char *argv[]) {
     string mcast_addr;
     string nazwa_odbiornika;
-    uint32_t data_port;
+    string data_port;
     uint32_t ctrl_port;
     uint32_t psize;
     uint32_t fsize;
